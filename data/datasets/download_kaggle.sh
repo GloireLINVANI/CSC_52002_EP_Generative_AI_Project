e860990ec8a195f235e4cafc5d0490b9291a2590
Kenneth Browder <kbrowder-24@gpu4.enst.fr>
2025-03-23 19:22:24 +0100

    add kaggle download script and masks

---
+++ data/datasets/download_kaggle.sh
@@ -0,0 +1,4 @@
+python -m pip install kaggle
+kaggle datasets download -d nautpanda/places-365-train-val-subset
+kaggle datasets download -d nautpanda/imagenet-subset
+kaggle datasets download -d nautpanda/nautpanda/coco-2017-subset
